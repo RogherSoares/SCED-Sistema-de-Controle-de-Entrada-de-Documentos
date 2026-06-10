4a68e05020c123b3b5c79528582727ed2d7a333a
Mateus Yano <mattochioyano98@gmail.com>
2026-06-10 18:24:52 -0300

    feat: implementa servico de envio de codigo por email

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
+import { EmailService } from '../auth/email.service';
 import { TwoFactorCodeEntity } from '../auth/entities/two-factor-code.entity';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
@@ -15,7 +16,10 @@ import { UsuarioEntity } from './entities/usuario.entity';
     AuthModule,
   ],
   controllers: [UsuariosController],
-  providers: [UsuariosService],
+  providers: [
+    UsuariosService,
+    EmailService,
+  ],
   exports: [UsuariosService],
 })
 export class UsuariosModule {}
