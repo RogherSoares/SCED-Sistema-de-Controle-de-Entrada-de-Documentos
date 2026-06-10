0662670af728177a27e98f109618518ec549236f
Mateus Yano <mattochioyano98@gmail.com>
2026-06-10 18:23:43 -0300

    feat: registra entidade de codigos 2fa no modulo de usuarios

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
+import { TwoFactorCodeEntity } from '../auth/entities/two-factor-code.entity';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { UsuarioEntity } from './entities/usuario.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsuarioEntity]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([
+      UsuarioEntity,
+      TwoFactorCodeEntity,
+    ]),
+    AuthModule,
+  ],
   controllers: [UsuariosController],
   providers: [UsuariosService],
   exports: [UsuariosService],
